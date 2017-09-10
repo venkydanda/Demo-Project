3e97f174afe0474e183100fa46fb86049845d51b
venky <venkys04.was@gmail.com>
2017-09-10 17:30:22 -0400

    added a change

--- src/main/java/com/pg/demoone/HelloPG.java
+++ src/main/java/com/pg/demoone/HelloPG.java
@@ -5,6 +5,7 @@ public class HelloPG {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Prathyusha Ganta, welcome..!!");
+       System.out.println("added a change");
 	}
 
 }
