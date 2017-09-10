54c460d6b2ca3c096ccf37cdbf096cc608af784f
venky <venkys04.was@gmail.com>
2017-09-10 17:25:53 -0400

    checkin

---
+++ src/main/java/com/pg/demoone/HelloPG.java
@@ -0,0 +1,10 @@
+package com.pg.demoone;
+
+public class HelloPG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Prathyusha Ganta, welcome..!!");
+	}
+
+}
